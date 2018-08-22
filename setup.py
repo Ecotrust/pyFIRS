f3a607ff102be86fb2ab1698e397c0457675e8b6
David Diaz <ddiaz@ecotrust.org>
2018-08-22 13:04:30 -0700

    Added author info an URL

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
     py_modules=['pyFIRS.utils'],
     license='BSD 3-Clause License',
     long_description=open('README.md').read(),
+    author='David Diaz',
+    author_email='ddiaz@ecotrust.org'
+    url='https://github.com/Ecotrust/pyFIRS'
 )
