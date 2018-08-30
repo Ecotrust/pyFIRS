d79c5f9ee8fc8cf2f00bac5bed825768083154f3
David Diaz <ddiaz@ecotrust.org>
2018-08-30 09:56:30 -0700

    Updates formatting of lastools kws to specify list-like arguments of a keyword separately

--- pyFIRS/utils/formatters.py
+++ pyFIRS/utils/formatters.py
@@ -5,23 +5,34 @@ def listlike(arg):
     return not hasattr(arg, "strip") and (hasattr(arg, "__getitem__") or
             hasattr(arg, "__iter__"))
 
-def format_lastools_kws(arg):
+def format_lastools_kws(**kwargs):
     '''Formats keyword arguments for LAStools command line usage'''
-    if isinstance(arg, bool):
-        return ''
-    elif listlike(arg):
-        return ' '.join(str(x) for x in arg)
-    else:
-        return str(arg)
+    kws = []
+    for key, value in kwargs.items():
+        if isinstance(value, bool):
+            kws.append('-{}'.format(key))
+        elif listlike(value):
+            kws.append('-{}'.format(key))
+            for arg in value:
+                kws.append(str(arg))
+        else:
+            kws.append('-{}'.format(key))
+            kws.append(str(value))
+    return kws
 
-def format_fusion_kws(arg):
+def format_fusion_kws(**kwargs):
     '''Formats keyword arguments for FUSION command line usage'''
-    if isinstance(arg, bool):
-        return ''
-    elif listlike(arg):
-        return ':'+",".join(str(x) for x in arg)
-    else:
-        return ':' + str(arg)
+    kws = []
+    for key, value in kwargs.items():
+        if isinstance(value, bool):
+            kws.append('/{}'.format(key))
+        elif listlike(value):
+            kws.append('/{}:'.format(key))
+            kws.append(','.join(str(x) for x in value))
+        else:
+            kws.append('/{}:'.format(key))
+            kws.append(str(value))
+    return kws
 
 def format_fusion_args(arg):
     '''Formats positional arguments for FUSION command line usage'''
